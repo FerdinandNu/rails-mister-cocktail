23264fc4042e066badbf4e0ee81e8f449b28e061
Ferdinand Nussbaum <ferdinand18.2n@gmail.com>
2019-02-28 12:52:02 +0100

    Model Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+require 'open-uri'
+
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+
+html_file = open(url).read
+data_ingredients = JSON.parse(html_file)
+
+data_ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
