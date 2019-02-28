22acb2be667f95294a6d59af242a942463dad8da
Ferdinand Nussbaum <ferdinand18.2n@gmail.com>
2019-02-28 19:14:55 +0100

    :S

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,45 @@
 class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id].to_i)
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(rest_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def rest_params
+    params.require(:cocktail).permit(:name)
+  end
+
+
+  # def create
+  #   @cocktails = Cocktail.new(cocktail_params)
+  #   if @cocktail.save
+  #     redirect_to cocktail_path(@cocktail)
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # private
+
+  # def cocktail_params
+  #   params.require(:cocktail).permit(:name)
+  # end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,22 @@
 class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(rest_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def rest_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
+
